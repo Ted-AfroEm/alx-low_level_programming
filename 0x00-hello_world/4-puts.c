1fc384282f88253cf6a771b55aa0801e72652471
Ted-AfroEm <wedikahsu122@gmail.com>
2022-03-10 15:19:08 +0300

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * Description - " Programming is like
+ * building a multilingual puzzle"
+ */
+ int main(void)
+ {
+     char message[]="Programming is like building a multilingual puzzle\n";
+     puts(message);
+     return (0);
+ }
