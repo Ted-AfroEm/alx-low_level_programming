7cf7e1ca4b3ac2683cc1eb50ad203b8330e6a643
Tedros Kahsu <tedkah2022@gmail.com>
2022-03-11 15:36:29 +0300

    typo

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,11 +16,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 if (n > 0)
-printf("%u is postive\n", n);
+printf("%i is positive\n", n);
 else if (n < 0)
-printf("%u is negative\n", n);
+printf("%i is negative\n", n);
 else
-printf("%u is zero\n", n);
+printf("%i is zero\n", n);
 
 return (0);
 }
