8ed36eef3e5e04aac770e0a86de791a3375f94b7
Tedros Kahsu <tedkah2022@gmail.com>
2022-03-11 17:20:25 +0300

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet game
+ *
+ * Description: prints all lower case letters
+ * staarting from a-z using putchar()
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+char letter;
+
+letter = 'a';
+
+while (letter <= 'z')
+{
+	if (letter != 'q' && letter != 'e')
+	{
+		putchar(letter);
+	}
+	letter++;
+}
+
+putchar('\n');
+
+return (0);
+}
