45b808f516b447706e82449e2945002cac180cd0
Tedros Kahsu <tedkah2022@gmail.com>
2022-03-11 16:48:02 +0300

    newline

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,11 +23,11 @@ lastDigit = n % 10;
 printf("Last digit of %i is %i ", n, lastDigit);
 
 if (lastDigit > 5)
-printf("and is greater than 5");
+printf("and is greater than 5\n");
 else if (lastDigit < 6 && lastDigit != 0)
-printf("and is less than 6 and not 0");
+printf("and is less than 6 and not 0\n");
 else
-printf("and is 0");
+printf("and is 0\n");
 
 return (0);
 }
