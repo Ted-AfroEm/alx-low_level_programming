dbaed8bd3653dc12b8726d0238d7745c1bf24c75
Ted-AfroEm <wedikahsu122@gmail.com>
2022-03-21 08:15:31 +0300

    Task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,6 @@
-#include <stdio.h>
-#include "_putchar.c"
+#ifndef MAIN_H
+#define MAIN_H
 
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
+int _putchar(char c);
+
+#endif
