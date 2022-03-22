b5489754676536f05b9911f95bf9730c0b0ddbb3
Ted-AfroEm <wedikahsu122@gmail.com>
2022-03-22 10:16:36 +0300

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - print each character followed by a newline
+ * @str: pointer for string
+ *
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		_putchar('\n');
+		str++;
+	}
+}
