7aa9877abbd2016291c34e9d3c7a8b2ecc8eb178
Tedros Kahsu <tedkah2022@gmail.com>
2022-03-23 04:40:10 +0300

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pinted by src to dest
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: *dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	do {
+		dest = &(*src);
+		dest++;
+		src++;
+	} while(*src != '\0');
+	return (dest);
+}
