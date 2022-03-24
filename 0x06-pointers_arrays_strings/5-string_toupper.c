46ea5c1ce7ef37dc9f744fe657845a740839ab40
Tedros Kahsu <tedkah2022@gmail.com>
2022-03-24 20:36:17 +0300

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,37 +1,25 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * string_toupper(char *s) - changes all lowercase letters of a string
+ * string_toupper - changes all lowercase letters of a string
  * to uppercase
  * @s: pointer to string
+ *
+ * Description: this is a function that change to uppercase
  *
  * Return: return s
  */
 char *string_toupper(char *s)
 {
-	char lower[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-			  'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-			  's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-	char upper[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-			  'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-			  'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 	char *p;
-	int i = 0;
 
 	p = s;
 	while (*s != '\0')
 	{
-		while (i < 26)
+		if (*s >= 97 && *s <= 122)
 		{
-			if(*s == lower[i])
-			{
-			       	lower[i] = upper[i];
-				printf("found %c", lower[i]);
-			       	break;
-			}
-			i++;
+			*s = *s - 32;
 		}
-		i = 0;
 		s++;
 	}
 
