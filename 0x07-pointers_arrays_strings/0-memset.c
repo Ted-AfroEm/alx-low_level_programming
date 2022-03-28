c420457fe84946d22f7668e734f4b738e909baa3
Ted-AfroEm <wedikahsu122@gmail.com>
2022-03-28 14:19:17 +0300

    initial commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first n bytes fo the memory area pointed to by s 
+ * with the constant byte b
+ * @s: string pointer
+ * @b: char vasue to fill
+ * @n: number of bytes of memory
+ *
+ * Return: s pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *p;
+	unsigned int i = 0;
+
+	p = s;
+	while (i < n || *s != '\0')
+	{
+		*s = b;
+		s++;
+		i++;
+	}
+	s = p;
+	return (s);
+}
