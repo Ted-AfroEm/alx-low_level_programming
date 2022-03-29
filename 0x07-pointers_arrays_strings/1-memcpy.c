97cd0e10a46b88751826e1f9baa83cef1ddbfd37
Tedros Kahsu <tedkah2022@gmail.com>
2022-03-29 16:28:44 +0300

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ * @dest: destination string pointer
+ * @src: source string pointer
+ * @n: number of bytes to copy
+ *
+ * Return: dest ponter
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *p;
+	unsigned int i;
+	p = dest;
+
+	while (i < n)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		i++;
+		if (*src == '\0')
+			break;
+	}
+	dest = p;
+	return dest;
+}
