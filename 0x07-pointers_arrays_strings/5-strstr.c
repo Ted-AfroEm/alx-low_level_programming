f21c96de7cb289b2edb7c0c56844adb7d8159a69
Tedros Kahsu <tedkah2022@gmail.com>
2022-03-29 18:07:25 +0300

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string
+ * @needle: the substring to locate
+ *
+ * Return: ponter to locate substring inside @haystack else NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+}
