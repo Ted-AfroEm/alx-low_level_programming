24a8466ba38f0412ce4bcc07cd64a170829d3268
Tedros Kahsu <tedkah2022@gmail.com>
2022-04-04 17:30:23 +0300

    Task 3 debug

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,12 +11,16 @@ int **alloc_grid(int width, int height)
 {
 	int **grid;
 	int i;
-	if( width <= 0 || height <= 0)
-		return NULL;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
 	grid = (int **) malloc(sizeof(int *) * height);
 	for (i = 0; i < height; i++)
 		grid[i] = (int *) malloc(sizeof(int) * width);
-	if(grid == NULL)
+	if (grid == NULL)
+	{
+		free(grid);
 		return (NULL);
+	}
 	return (grid);
 }
