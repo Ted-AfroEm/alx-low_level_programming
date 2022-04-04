b209cb8a2a3b9f7d1cd255bbcb9ea4ca9cb46cd0
Tedros Kahsu <tedkah2022@gmail.com>
2022-04-05 02:00:41 +0300

    initial commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * creat_array - creat an array of chars
+ * @size: unsigned int
+ * @c: char
+ *
+ * Return: char pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	if(size == 0)
+	{
+		return NULL;
+	}
+}
