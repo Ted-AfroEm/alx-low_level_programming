2b33ad4e29ae3398b698ef14b07f54ef190a2825
Tedros Kahsu <tedkah2022@gmail.com>
2022-04-12 10:23:26 +0300

    Task 1

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
+#include "dog.h"
+struct dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
