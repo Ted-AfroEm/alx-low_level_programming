582d77ebc8921677cd53e15258460e1751fbfa35
Ted-AfroEm <wedikahsu122@gmail.com>
2022-04-16 08:40:54 +0300

    correct the error there is only one argument

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,12 +8,13 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int  sum = n;
+	int  sum = 0;
 	unsigned int i;
 	va_list ptr;
 
+	if (n == 0)
+		return (0);
 	va_start(ptr, n);
-
 	for (i = 0; i < n; i++)
 		sum += va_arg(ptr, int);
 	va_end(ptr);
