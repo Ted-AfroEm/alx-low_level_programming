9ba9edf08f573116d6a014582a334f85a1ce4be0
Tedros Kahsu <wedikahsu122@gmail.com>
2022-04-21 18:50:13 +0300

    add task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * print_list - print all the elements in a list
+ * @h: pointer to a list
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+}
