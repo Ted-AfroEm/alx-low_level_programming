40dfe2eb835be3736e41b0096873f0a8912658d8
Tedros Kahsu <wedikahsu122@gmail.com>
2022-04-23 08:09:53 +0300

    update islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - return 1 if true
+ * @c:ascii code
+ *
+ * Return: 1 if true else 0
+ */
 int _islower(int c)
 {
-  if(c > 97 && c < 122)
-    return 1;
-  else
-    return 0;
+	if (c > 97 && c < 122)
+		return (1);
+	else
+		return (0);
 }
