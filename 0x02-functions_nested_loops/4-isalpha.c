4bc07081e7c79f6a7f30a4c744546aef49944397
Tedros Kahsu <wedikahsu122@gmail.com>
2022-04-23 08:16:04 +0300

    add task-4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if letter
+ * @c: ascii code
+ *
+ * Return: 1 if true else 0
+ */
 int _isalpha(int c)
 {
-  if(c >= 65 && c <=90 || c >= 97 && c <=122)
-    return 1;
-  else
-    return 0;
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
 }
- 
