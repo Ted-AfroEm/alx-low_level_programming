aed726c77a22399a22610fad5425fc1acb84d113
Tedros Kahsu <wedikahsu122@gmail.com>
2022-04-23 08:03:13 +0300

    print a-z 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,24 @@
-void print_alphabet_x10()
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabets 10 times
+ *
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
 {
-  char letter = 'a';
-  int i = 0;
-  while(i < 10)
-    {
-      while(letter <= 'z')
-     {
-       _putchar(letter);
-       letter++;
-    }
-  _putchar('\n');
-  letter = 'a';
-  i++;
-    }
+	char letter = 'a';
+	int i = 0;
+
+	while (i < 10)
+	{
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+		letter = 'a';
+		i++;
+	}
 }
