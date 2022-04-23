276953abd9fac7da2df36041c539637dece284b3
Tedros Kahsu <wedikahsu122@gmail.com>
2022-04-23 07:58:08 +0300

    print alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,18 @@
-void print_alphabet()
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return: nothing
+ */
+void print_alphabet(void)
 {
-  char letter = 'a';
-  while(letter <= 'z')
-    {
-      _putchar(letter);
-      letter++;
-    }
-  _putchar('\n');
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
 }
