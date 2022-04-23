f6b626350aa040760e949fea2d556b95e6773d02
Tedros Kahsu <wedikahsu122@gmail.com>
2022-04-23 08:27:11 +0300

    add task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,18 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - print sign of a number
+ * @n: number
+ *
+ * Return: sign of a number
+ */
 int print_sign(int n)
 {
-  if(n > 0)
-    {
-    _putchar('+');
-    return 1;
-    }
-  else if(n == 0)
-    {
-    _putchar('0');
-    return 0;
-    }
-  else
-    {
-    _putchar('-');
-    return -1;
-    }
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
